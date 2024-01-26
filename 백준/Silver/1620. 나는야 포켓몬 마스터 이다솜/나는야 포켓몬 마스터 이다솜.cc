8858817f4a766ca21797eb34c70caa79a03e2e79
moooooooonlight <128355951+moooooooonlight@users.noreply.github.com>
2024-01-26 16:18:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 148 ms, Memory: 28512 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,31 +1,44 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
 using namespace std;
 
-int n, m;
-string str;
-map<string, int> num;
-string name[100001];
-
-int main() {
+int main(){
   ios::sync_with_stdio(false);
   cout.tie(NULL);
   cin.tie(NULL);
 
-  cin >> n >> m;
-  for (int i = 1; i <= n; i++) {
-    cin >> str;
-    num.insert({ str, i });
-    name[i] = str;
+  int n,m;
+  string str;
+  map<string, int> h;
+  vector<string> name;
+  vector<string> result; 
+
+  cin>>n>>m;
+  for(int i=1;i<=n;++i){
+    cin>>str;
+    name.push_back(str);
+    h.insert({str,i});
   }
-  for (int i = 1; i <= m; i++) {
-    cin >> str;
-    if (isdigit(str[0])) {
-      cout << name[stoi(str)] << '\n';
-    }
-    else {
-      cout << num.find(str)->second << '\n';
+
+
+
+
+
+  
+  for(int i=0;i<m;++i){
+    cin>>str;
+    if(str[0]>=65 && str[0]<=90){
+      result.push_back(to_string(h[str]));
+    }else{
+      result.push_back(name[stoi(str)-1]);
     }
   }
+
+  for (int i = 0; i < result.size();i++){
+      cout << result[i] << '\n'; 
+  }
+  
+  return 0;
 }
