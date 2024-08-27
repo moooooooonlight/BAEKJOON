26279eb488dd45aa8f49b746fc43b243a9f193e6
moooooooonlight <128355951+moooooooonlight@users.noreply.github.com>
2024-08-28 08:34:33 +0900

    [Gold V] Title: 용액, Time: 64 ms, Memory: 3568 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,62 @@
+// 두 용액을 합쳐서 특성값이 0에 가까운 값 만들기
+// 두 용액을 찾아야함.
+// 백트레킹?
+// 0에 가장 가까운 값을 구하는거.
+// 최대 = 벡터 정렬 후 맨 끝 2숫자 더하기
+// 최소 -99 최대 98.
+// 만약에 최소값이 더 커 -> 왼쪽에서 움직임.
+// 만약에 최대값이 더 커 -> 오른쪽에서 움직임.
+// 3 -3
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int n;
+long long Left, Right, MIN=1000000000;
+long long result_L, result_R;
+vector<long long> v;
+
+void input(){
+  cin>>n;
+  int x;
+  for(int i=0;i<n;i++){
+    cin>>x;
+    v.push_back(x);
+  }
+}
+
+void solve(){
+  Left = 0;
+  Right = n-1;
+
+
+  int sum1 = abs(v[Left] + v[Right]);
+  result_L= v[Left] ;
+  result_R = v[Right]; 
+
+  
+  while(Left < Right){
+    int sum = v[Left] + v[Right];
+    if(abs(sum)<sum1){
+        sum1 = abs(sum);
+      result_L = v[Left];
+      result_R = v[Right];
+    }
+    
+    if(sum<0) {
+      Left++;
+    }else{
+      Right--;
+    }
+  }
+}
+
+int main(){
+  input();
+  solve();
+  cout<<result_L<<" "<<result_R<<"\n";
+  return 0;
+}
